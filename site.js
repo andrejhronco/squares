05c69f2e23ca4394b9612e1ea1e713381711c415
Andrej Hronco <andrejhronco@gmail.com>
2012-09-05 20:48:19 -0700

    Added simple option
    Added simple option and option to set square rot range.

--- site.js
+++ site.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-	var randomNumber = randomFromTo(2,100, '.root-question',true); // first run on load
+	var simple = true;
+	$('input.simple').change(function(){
+		simple = this.checked ? true : false;
+		return simple;
+	});
+
+	var randomNumber = randomFromTo(2,25, '.root-question', simple); // first run on load
 	var randNum = randomNumber[1]; // set randNum var
-	
-	//$('.square-root').text(Math.sqrt(randNum)+' : '+Math.floor(Math.sqrt(randNum))); // this shows the answer on load
-	
+		
 	$('.next').on('click', function(){ // asks another question
-		randomNumber = randomFromTo(2,100, '.root-question',true); //calls randomFromTo();
+		randomNumber = randomFromTo(2,25, '.root-question', simple); //calls randomFromTo();
 		randNum = randomNumber[1]; //sets/updates the randNum var
 	});
 //________ end next root question _________\\
@@ -13,11 +17,11 @@ $(document).ready(function() {
 		if($("input[name=answer]").val() == Math.sqrt(randomNumber[1])) {
 			$('.square-root').text('correct!');
 			$('#box').createBoxes(1600, 5);
-	        return false;
-	    }
+			return false;
+		}
 		else{
 			$('.square-root').text('wrong!');
-	      	return false;
+			return false;
 		}
 	});
 //________ end submit  _________\\
@@ -25,28 +29,31 @@ $(document).ready(function() {
 
 });
 function randomFromTo(from, to, el, simple){
-	var  randNum, simpleSet;
+	var  randNum, simpleSet, fromTo = $('.from-to');
+
 	if(simple === true){
+		fromTo.hide();
 		simpleSet = [4,9,16,25,36,49,64,81,100,121,144,169,196,225];
 		randNum = simpleSet[Math.floor(Math.random()*simpleSet.length)];
 	}
 	else {
-		randNum =  Math.floor(Math.random() * (to - from + 1) + from);
+		fromTo.show();
+		randNums =  Math.floor(Math.random() * (to - from + 1) + from);
+		randNum = randNums*randNums;
 	}
 	return [$(el).text(randNum), randNum];
 }
-
-//________ end randSquare _________\\
+function isInt(n) {
+   return n % 1 === 0;
+}
+//________ end fromTo _________\\
 
 $.fn.createBoxes = function(numberOfBoxes, boxSize){
 	$this = $(this);
 	var box = [], boxClass, ranNum = Math.floor(Math.random() * 100);
-	//console.log(ranNum);
+
 	for(i=1;i<=numberOfBoxes;i++){
-		// if(i%3 === 0) boxClass = 'black';
-		// if(i%5 === 0) boxClass = 'white';
-		// if(i%15 === 0) boxClass = 'gray';
-		(i%ranNum === 0 || i%ranNum === 2) ? boxClass = 'black' : boxClass = 'white';
+		(i%ranNum === 2) ? boxClass = 'black' : boxClass = 'white';
 		if(i%ranNum === 0) boxClass = 'gray';
 		box.push("<div class='box size-"+boxSize+" box-"+i+" "+boxClass+"'></div>");
 	}
